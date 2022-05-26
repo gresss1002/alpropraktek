227263c7c8efe7e0c921d67876e3110c67e384be
gress1002 <agrestina01.gmail.com>
2022-05-26 18:26:58 +0700

    program pemain ambil buah

---
+++ let seragamSekolah;.js
@@ -0,0 +1,24 @@
+let seragamSekolah;
+let hari = "sabtu";
+
+switch (hari) {
+  case "senin":
+    seragamSekolah = "kemeja putih bawahan merah";
+    break;
+  case "selasa":
+    seragamSekolah = "kemeja hijau bawahan hitam";
+    break;
+  case "rabu":
+    seragamSekolah = "kemeja putih bawahan putih";
+    break;
+  case "kamis":
+    seragamSekolah = "kemeja batik merah bawahan hitam";
+    break;
+  case "jumat":
+    seragamSekolah = "baju pramuka";
+    break;
+  default:
+    seragamSekolah = "baju bebas";
+}
+
+console.log(seragamSekolah); 
